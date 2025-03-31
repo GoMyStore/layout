8d4880ee9b22adfcf352996122a9252c3deeef9e
Mhirii <ahmedmhiri218@gmail.com>
2025-03-31 01:43:39 +0100

    add nats jetstream

--- internal/dep/nats.go
+++ internal/dep/nats.go
@@ -1,16 +1,20 @@
 package dep
 
 import (
+	"context"
+
 	"layout/internal/conf"
 
 	"github.com/go-kratos/kratos/v2/errors"
 	"github.com/go-kratos/kratos/v2/log"
 	"github.com/nats-io/nats.go"
+	"github.com/nats-io/nats.go/jetstream"
 	"go.opentelemetry.io/otel/trace"
 )
 
 type Nats struct {
 	Conn   *nats.Conn
+	JS     jetstream.JetStream
 	Logger *log.Helper
 	tp     trace.Tracer
 }
@@ -18,20 +22,38 @@ type Nats struct {
 func NewNats(c *conf.Bootstrap, logger log.Logger, tp trace.TracerProvider) (*Nats, error) {
 	t := tp.Tracer("nats")
 	log := log.NewHelper(logger)
+	var conn *nats.Conn
 
-	conn, err := conn(c, log)
+	conn, err := connect(c, log, t, context.Background())
 	if err != nil {
 		return nil, err
 	}
 
-	return &Nats{
+	nats := Nats{
 		Logger: log,
 		tp:     t,
 		Conn:   conn,
-	}, nil
+	}
+
+	if c.GetData().GetNats().GetJetstream() {
+		js, err := nats.connectJS(c)
+		if err != nil {
+			return nil, err
+		}
+		return &Nats{
+			Logger: log,
+			tp:     t,
+			Conn:   conn,
+			JS:     js,
+		}, nil
+	}
+
+	return &nats, nil
 }
 
-func conn(c *conf.Bootstrap, log *log.Helper) (*nats.Conn, error) {
+func connect(c *conf.Bootstrap, log *log.Helper, t trace.Tracer, ctx context.Context) (*nats.Conn, error) {
+	ctx, span := t.Start(ctx, "dep.Nats.conn")
+	defer span.End()
 	addr := nats.DefaultURL
 	if c.GetData().GetNats().GetAddr() == "" {
 		return nil, errors.InternalServer("No nats address provided", "No data.nats.addr was set in the config")
@@ -53,4 +75,26 @@ func conn(c *conf.Bootstrap, log *log.Helper) (*nats.Conn, error) {
 	defer nc.Close()
 
 	return nc, nil
+}
+
+func (n *Nats) connectJS(c *conf.Bootstrap) (jetstream.JetStream, error) {
+	ctx, span := n.tp.Start(context.Background(), "dep.Nats.connJS")
+	defer span.End()
+
+	var conn *nats.Conn = n.Conn
+	var err error
+
+	if conn == nil {
+		conn, err = connect(c, n.Logger, n.tp, ctx)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	js, err := jetstream.New(conn)
+	if err != nil {
+		return nil, err
+	}
+
+	return js, nil
 }
